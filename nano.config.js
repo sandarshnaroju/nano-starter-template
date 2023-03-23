b42bbdd5c08f361daead844e9b71ae5180dea03f
Sandarsh Naroju <narojusandarsh18@gmail.com>
2023-03-23 19:37:45 +0530

    added nano-config

---
+++ nano.config.js
@@ -0,0 +1,17 @@
+const newTable = {
+  name: 'new',
+  properties: {
+    key: 'string',
+    value: 'string',
+  },
+  primaryKey: 'key',
+};
+
+export const DataBaseConfig = {
+ // schema: [newTable],
+ // schemaVersion: 3,
+};
+export const CLIENT_ID = 'client-Id';
+export const CLIENT_SECRET =
+  'Client-secret';
+export const APP_URL = 'app url';
