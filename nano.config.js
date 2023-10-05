fb5dad2fc6a3ac791537fed8abada93e22fdcfa0
sandarshnaroju <72196193+sandarshnaroju@users.noreply.github.com>
2023-10-05 23:35:00 +0530

    Updated nano.config.js

--- nano.config.js
+++ nano.config.js
@@ -14,3 +14,25 @@ export const DataBaseConfig = {
 export const CLIENT_ID = 'client-Id';
 export const CLIENT_SECRET = 'Client-secret';
 export const APP_URL = 'app url';
+export const RELOAD_TIME = 10000; 
+ const lightTheme = {
+  name: "light",
+  isDark: false,
+  colors: {
+    primary: "blue",
+    secondary: "#800460",
+    background: "#e0f4f4",
+  },
+};
+const darkTheme = {
+  name: "dark",
+  isDark: true,
+  colors: {
+    primary: "black",
+    secondary: "yellow",
+    background: "gray",
+  },
+};
+
+export const THEMES = [lightTheme, darkTheme];
+  
